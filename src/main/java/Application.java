07df98b81016e3af746b85657c8b1c8ee3583a60
Dan Baker <bakerz2009@gmail.com>
2015-02-18 21:04:48 -0700

    Setup backend

---
+++ src/main/java/Application.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Created by dan on 2/18/15.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@EnableJpaRepositories
+@EntityScan
+public class Application extends WebMvcAutoConfiguration {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+}
