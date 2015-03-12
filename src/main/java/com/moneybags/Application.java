114c6349a947e66bfeb5f16d3391f353f1a60682
Dan Baker <bakerz2009@gmail.com>
2015-03-11 22:39:52 -0600

    Updated spring boot version

--- src/main/java/com/moneybags/Application.java
+++ src/main/java/com/moneybags/Application.java
@@ -1,3 +1,5 @@
+package com.moneybags;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
